2811addb61a68ecec7fabb121530866c91a0a3d7
lfrountzos <lfrountzos@gmail.com>
2014-11-24 23:33:33 +0200

    ccx

--- DocumentManager/src/com/example/documentmanager/DocUI.java
+++ DocumentManager/src/com/example/documentmanager/DocUI.java
@@ -1,9 +1,12 @@
 package com.example.documentmanager;
 
 import java.io.File;
+import java.util.List;
 
 import javax.servlet.annotation.WebServlet;
 
+import com.myPkg.baseDTOs.myEnglish.IrregularVerb;
+import com.myPkg.services.myEnglishImpl.IrregularVerbImpl;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.data.Property.ValueChangeEvent;
@@ -13,10 +16,12 @@ import com.vaadin.data.util.TextFileProperty;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.shared.ui.label.ContentMode;
+import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.HorizontalSplitPanel;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Table;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalSplitPanel;
 
 @SuppressWarnings("serial")
 @Theme("documentmanager")
@@ -24,7 +29,10 @@ public class DocUI extends UI {
 
 	FilesystemContainer docs = new FilesystemContainer(new File("E:\\DocsAndOthers\\DataForApplicationTests\\tmp\\docs"));
 	Table docList = new Table("Documents", docs);
-	Label docView = new Label("", ContentMode.HTML);
+	DocEditor docView = new DocEditor();
+	
+	List<IrregularVerb> lst = new IrregularVerbImpl().getIrregularVerb();
+	ComboBox iregList = new ComboBox("Irregular", lst);
 	
 	@WebServlet(value = "/*", asyncSupported = true)
 	@VaadinServletConfiguration(productionMode = false, ui = DocUI.class)
@@ -33,11 +41,14 @@ public class DocUI extends UI {
 
 	@Override
 	protected void init(VaadinRequest request) {
+		HorizontalSplitPanel hSplit = new HorizontalSplitPanel();
+		VerticalSplitPanel vSplit = new VerticalSplitPanel();
+		setContent(hSplit);
+		hSplit.addComponent(vSplit);
+		hSplit.addComponent(iregList);
+		vSplit.addComponent(docList);
+		vSplit.addComponent(docView);
 		
-		HorizontalSplitPanel split = new HorizontalSplitPanel();
-		setContent(split);
-		split.addComponent(docList);
-		split.addComponent(docView);
 		docList.setSizeFull();
 
 		docList.addValueChangeListener(new ValueChangeListener() {
